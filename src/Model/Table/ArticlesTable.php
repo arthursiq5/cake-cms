c06b9392edb32d1fd753c4de64650f8481476928
Arthur Siqueira <arthur.siq5@gmail.com>
2021-01-25 15:40:13 -0300

    criada inserção básica de artigos

--- src/Model/Table/ArticlesTable.php
+++ src/Model/Table/ArticlesTable.php
@@ -1,8 +1,12 @@
 <?php
-
+// src/Model/Table/ArticlesTable.php
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+// the Text class
+use Cake\Utility\Text;
+// the EventInterface class
+use Cake\Event\EventInterface;
 
 class ArticlesTable extends Table
 {
@@ -10,4 +14,12 @@ class ArticlesTable extends Table
     {
         $this->addBehavior('Timestamp');
     }
+    public function beforeSave(EventInterface $event, $entity, $options)
+{
+    if ($entity->isNew() && !$entity->slug) {
+        $sluggedTitle = Text::slug($entity->title);
+        // trim slug to maximum length defined in schema
+        $entity->slug = substr($sluggedTitle, 0, 191);
+    }
+}
 }
